ae4293d5858cd89e1265ed9242562b9198c87a98
Jaloliddin-Fozilov <jaloliddinish@gmail.com>
2022-08-09 14:19:08 +0500

    styles working

--- themes/carit/inc/scripts.php
+++ themes/carit/inc/scripts.php
@@ -5,13 +5,19 @@
  */
 function carit_scripts() {
 
-	wp_enqueue_style( 'enhance', get_template_directory_uri() . '/assets/css/enhance.css', array(), _S_VERSION, true );
-	wp_enqueue_style( 'catalog-enhance', get_template_directory_uri() . '/assets/css/catalog-enhance.css', array(), _S_VERSION, true );
-	wp_enqueue_style( 'fonts', get_template_directory_uri() . '/assets/css/fonts.css', array(), _S_VERSION, true );
-	wp_enqueue_style( 'main-style', get_template_directory_uri() . '/assets/css/main-style.css', array(), _S_VERSION, true );
-	wp_enqueue_style( 'hot-ideas', get_template_directory_uri() . '/assets/css/hot-ideas.css', array(), _S_VERSION, true );
-	wp_enqueue_style( 'product-type-shifter-block', get_template_directory_uri() . '/assets/css/product-type-shifter-block.css', array(), _S_VERSION, true );
-	wp_enqueue_style( 'universal-tabs', get_template_directory_uri() . '/assets/css/universal-tabs.css', array(), _S_VERSION, true );
+	wp_enqueue_style( 'cart', get_template_directory_uri() . '/assets/css/cart.css', array(), true );
+	wp_enqueue_style( 'pressroom', get_template_directory_uri() . '/assets/css/pressroom.css', array(), true );
+	wp_enqueue_style( 'wt-ptype-grid', get_template_directory_uri() . '/assets/css/wt-ptype-grid.css', array(), true );
+	wp_enqueue_style( 'static-pages', get_template_directory_uri() . '/assets/css/static-pages.css', array(), true );
+	wp_enqueue_style( 'product-enhance', get_template_directory_uri() . '/assets/css/product-enhance.css', array(), true );
+	wp_enqueue_style( 'product-navigation', get_template_directory_uri() . '/assets/css/product-navigation.css', array(), true );
+	wp_enqueue_style( 'enhance', get_template_directory_uri() . '/assets/css/enhance.css', array(), true );
+	wp_enqueue_style( 'catalog-enhance', get_template_directory_uri() . '/assets/css/catalog-enhance.css', array(), true );
+	wp_enqueue_style( 'fonts', get_template_directory_uri() . '/assets/css/fonts.css', array(), true );
+	wp_enqueue_style( 'main-style', get_template_directory_uri() . '/assets/css/main-style.css', array(), true );
+	wp_enqueue_style( 'hot-ideas', get_template_directory_uri() . '/assets/css/hot-ideas.css', array(), true );
+	wp_enqueue_style( 'product-type-shifter-block', get_template_directory_uri() . '/assets/css/product-type-shifter-block.css', array(), true );
+	wp_enqueue_style( 'universal-tabs', get_template_directory_uri() . '/assets/css/universal-tabs.css', array(), true );
 
 	wp_enqueue_style( 'carit-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'carit-style', 'rtl', 'replace' );
